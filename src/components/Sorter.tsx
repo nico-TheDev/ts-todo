f8fb39e97dfd5ed5bfab59f0191a9c7ee71bf4e0
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-12-05 20:11:17 +0800

    add sorter comp

---
+++ src/components/Sorter.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface IProps {}
+
+const Sorter: React.FC<IProps> = () => {
+    return (
+        <div className="flex justify-center items-center w-90 bg-white shadow-md rounded-md mx-auto p-4  text-gray-500 text-center text-sm">
+            <button className="text-blue-500">All</button>
+            <button className="ml-4">Active</button>
+            <button className="ml-4">Completed</button>
+        </div>
+    );
+};
+
+export default Sorter;
